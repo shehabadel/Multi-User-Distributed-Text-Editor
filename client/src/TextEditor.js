24d047f1d5d91cc1bf6882ac6cf5d073bf322fc9
Farah2001 <famr14478@gmail.com>
2022-05-13 02:14:25 +0200

    Updated Client Code

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -3,17 +3,29 @@ import "quill/dist/quill.snow.css"
 import { useCallback, useEffect,useRef, useState } from "react"
 import Quill from "quill"
 
+
+
+
 export default function TextEditor() {
-  const wrapper= useRef()
-    
-    useEffect(() => {
+    const wrapperReference= useCallback(wrapper => {
+        if(wrapper==null) return
+        wrapper.innerHTML=""
         const editor = document.createElement("div")
-        wrapper.current.append(editor)
-        new Quill(editor, {theme: "snow"})
-        return () => {
-            wrapper.innerHTML =""
-        }
+        wrapper.append(editor)
+        new Quill(editor, {theme: "snow", modules:{toolbar:ToolBarArea}})
+        
   }, []) 
-  return <div id="container" ref={wrapper}></div>
+  return <div className="container" ref={wrapperReference}></div>
   
 }
+const ToolBarArea = [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ font: [] }], //Default font used in quill
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["bold", "italic", "underline"],
+    [{ color: [] }, { background: [] }],
+    [{ script: "sub" }, { script: "super" }],
+    [{ align: [] }],
+    ["image", "blockquote", "code-block"],
+    ["clean"],
+  ]
